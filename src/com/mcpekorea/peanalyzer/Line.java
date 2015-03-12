a1b4fd73580c23ee68abe039375f8893ad30eb21
ChalkPE <amato0617@gmail.com>
2015-03-12 22:34:36 +0900

    Second code drop

--- src/com/mcpekorea/peanalyzer/Line.java
+++ src/com/mcpekorea/peanalyzer/Line.java
@@ -16,6 +16,20 @@ public abstract class Line implements Serializable {
 	}
 	
 	@Override
+	public boolean equals(Object o){
+		return o instanceof Line ? this.offset.equals(((Line) o).getOffset()) : false;
+	}
+	
+	@Override
+	public String toString(){
+		return offset.toString();
+	}
+	
+	public String toString(int length){
+		return offset.toString(length);
+	}
+	
+	@Override
 	public int hashCode(){
 		return offset.hashCode();
 	}
